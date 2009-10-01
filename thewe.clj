f38208315c1bf3fd12de8b0186dd3f03c4a4383b
Avital Oliver <avital@avital-laptop-2.(none)>
2009-10-01 17:24:30 +0200

    ALMOST ALMOST DONE (unit tests still need to be written)

--- thewe.clj
+++ thewe.clj
@@ -7,148 +7,203 @@
   (:use clojure.set)
   (:use compojure))
 
-; db is a map between replocs and blipData (= the event json we get from wave)
-(def db (atom {}))
-(def sync-table (atom #{}))
-(def log-buffer (atom ""))
+
+; =========================
+; ======= Utilities =======
+; =========================
+
+; (dig {:a {:b 3}} :a :b) returns 3
+(defn dig [map & rest]
+  (get-in map rest))
 
 
-;(defmacro defn [name- args & body]
-;  `(defn ~name- ~args
-;    (let [return-value# (do ~@body)]
-;      return-value#)))
+; =============================
+; ======= Logical Layer =======
+; =============================
 
-(defn log [& what]
-;  (if (coll? (second what)) (do (print (count what)) doall (second what)))
-;  (try (doall (second what))) ;@todo
-  (println what)
-;  (swap! log-buffer str what "\n\n")
-  (last what))
+; Data structures:
+; ----------------
+;
+; rep-loc:    Either {:type "blip" :wave-id wave-id :wavelet-id wavelet-id :blip-id blip-id} or
+;                    {:type "gadget" :wave-id wave-id :wavelet-id wavelet-id :blip-id blip-id :key key} or
+;
+; rep-op:     {:rep-loc rep-loc :content content}
+;
+; rep-rules:  A set of sets (a partition) of rep-locs
 
-(defn substring [a b]
-  (.substring a b))
 
-(defn read-and-clean-log []
-  (let [old-buffer @log-buffer]
-    (swap! log-buffer substring (count old-buffer))
-    old-buffer))
-
-(defmacro forex [seq-exprs & body-expr]
-  `(apply concat (for ~seq-exprs (list ~@body-expr))))
-
-(defn pairs-to-map [pairs]
-  (zipmap (map first pairs) (map second pairs)))
-
-(defn map-map [f m]
-  (pairs-to-map (map #(f (first %) (second %)) m)))
+; Receives rep-rules and incoming rep-ops and returns rep-ops to be acted upon
+(defn do-replication [rep-rules rep-ops]
+  (for [rep-op rep-ops
+        rep-class rep-rules :when (contains? rep-class (:rep-loc rep-op))
+        rep-loc rep-class :when (not= rep-loc (:rep-loc rep-op))]
+    {:rep-loc rep-loc :content (:content rep-op)}))
 
 
 
-(defn blip-data-to-db-structure [blip-id blip-data]
-  (let [basic-reploc {:wave-id (blip-data "waveId"), :wavelet-id (blip-data "waveletId"), :blip-id blip-id}]
-    (if-let [gadget-map (first (get-in blip-data ["elements" "map"]))]
+
+; ===============================================
+; ======= Google Wave Incoming JSON Layer =======
+; ===============================================
+
+; Data structures:
+; ----------------
+;
+; incoming-map:       Some crazy Google format of a map that contains information on which
+;                     blips were modified, their content (including gadgets inside)
+;                     and their parent blip contents
+;
+; blip-data:          (dig incoming-wave-map "blips" "map" blip-id)
+
+
+(defn blip-data-to-rep-ops [blip-data]
+  (let [basic-rep-loc {:wave-id (blip-data "waveId"), :wavelet-id (blip-data "waveletId"), :blip-id (blip-data "blipId")}]
+    (if-let [gadget-map (first (dig blip-data "elements" "map"))]
       ; there is a gadget here
-      (for [[key val] (get-in (val gadget-map) ["properties" "map"])]
-        [(assoc basic-reploc :type "gadget" :key key) val])
+      (let [gadget-state (dig (val gadget-map) "properties" "map")]
+        (swap! gadget-db assoc basic-rep-loc gadget-state)
+        (for [[k v] gadget-state]
+          {:rep-loc (assoc basic-rep-loc :type "gadget" :key k) :content v}))
+
       ; there is no gadget
-      [[(assoc basic-reploc :type "blip") (blip-data "content")]]
+      [{:rep-loc (assoc basic-rep-loc :type "blip") :content (blip-data "content")}]
     )
   )
 )
 
-(defn disj1 [set x]
-  (if set
-    (disj set x)
-    nil))
 
-; updates has the same structure as db (see db comment)
-; returns a map from reploc to its new content
-(defn updates-to-log-opers [sync-table updates]
-  (into {} (for [update updates
-        sync-class sync-table :when (contains? sync-class (key update))
-        reploc sync-class :when (not= reploc (key update))]
-    [reploc (val update)])))
+(defn incoming-map-to-rep-ops [incoming]
+  (let [modified-blip-ids 
+        (for [event (dig incoming "events" "list")
+              :when (not (.endsWith (event "modifiedBy") "@a.gwave.com"))
+              :when (= (event "type") "BLIP_SUBMITTED")]
+          (dig event "properties" "map" "blipId"))]
+    (apply concat
+      (for [blip-id modified-blip-ids]
+        (blip-data-to-rep-ops
+          (dig incoming "blips" "map" blip-id))))))
 
 
-(def log-reploc {:wave-id "wavesandbox.com!w+TeZzxdl4%A"
-                 :wavelet-id "wavesandbox.com!conv+root"
-                 :blip-id "b+TeZzxdl4%B"})
 
-(defn conj1 [x y] x)
-(defn conj1 [x y] (conj x y))
+; ===============================================
+; ======= Google Wave Outgoing JSON Layer =======
+; ===============================================
 
-(defn log-opers-to-json [log-opers]
+; Data structures:
+; ----------------
+;
+; outgoing-map:  Some crazy Google format of a map that contains information on which
+;                operations the robot will do
+
+(defn rep-op-to-operation [rep-op]
+  (let [rep-loc (rep-op :rep-loc) wave-id (:wave-id rep-loc) wavelet-id (:wavelet-id rep-loc) blip-id (:blip-id rep-loc) content (:content rep-op)]
+  (if (= (rep-loc :type) "blip")
+    [{
+      "index"  -1,
+      "waveletId"  wavelet-id,
+      "blipId"  blip-id,
+      "javaClass"  "com.google.wave.api.impl.OperationImpl",
+      "property"  nil,
+      "waveId"  wave-id,
+      "type"  "DOCUMENT_DELETE"
+      }
+     {
+      "index"  0,
+      "waveletId"  wavelet-id,
+      "blipId"  blip-id,
+      "javaClass"  "com.google.wave.api.impl.OperationImpl",
+      "property"  content,
+      "waveId"  wave-id,
+      "type"  "DOCUMENT_APPEND"
+      }]
+    ; else this is a gadget
+    [{
+      "index" 1,
+      "waveletId" wavelet-id,
+      "blipId" blip-id,
+      "javaClass" "com.google.wave.api.impl.OperationImpl",
+      "property" {
+                  "javaClass" "com.google.wave.api.Gadget",
+                  "properties" {
+                                "map" (assoc (@gadget-db (dissoc rep-loc :key :type)) (:key rep-loc) content
+                                "javaClass" "java.util.HashMap"
+                                },
+                  "type" "GADGET"
+                  },
+      "waveId" wave-id,
+      "type" "DOCUMENT_ELEMENT_REPLACE"
+      }]))))
+
+
+
+(defn rep-ops-to-outgoing-map [rep-ops]
   {
-	"javaClass"  "com.google.wave.api.impl.OperationMessageBundle",
-	"operations"  {
-		"javaClass"  "java.util.ArrayList",
-		"list"  (conj1 (forex [log-oper log-opers :let [reploc (key log-oper)]]
-			{
-				"index"  -1,
-				"waveletId"  (:wavelet-id reploc),
-				"blipId"  (:blip-id reploc),
-				"javaClass"  "com.google.wave.api.impl.OperationImpl",
-				"property"  nil,
-				"waveId"  (:wave-id reploc),
-				"type"  "DOCUMENT_DELETE"
-			}
-			{
-				"index"  0,
-				"waveletId"  (:wavelet-id reploc),
-				"blipId"  (:blip-id reploc),
-				"javaClass"  "com.google.wave.api.impl.OperationImpl",
-				"property"  (val log-oper),
-				"waveId"  (:wave-id reploc),
-				"type"  "DOCUMENT_APPEND"
-			})
-                        {
-				"index"  0,
-				"waveletId"  (:wavelet-id log-reploc),
-				"blipId"  (:blip-id log-reploc),
-				"javaClass"  "com.google.wave.api.impl.OperationImpl",
-				"property"  (read-and-clean-log),
-				"waveId"  (:wave-id log-reploc),
-				"type"  "DOCUMENT_APPEND"
-                         ; @todo MAKE THESE FUNCTIONS
-                        })
-            }
-	"version"  "101"   ; @todo WTF
-})
-
-(defn containsex? [big small]
-  (some #{small} big))
+   "javaClass"  "com.google.wave.api.impl.OperationMessageBundle",
+   "operations"  {
+                  "javaClass"  "java.util.ArrayList",
+                  "list"  (apply concat (map rep-op-to-operation rep-ops))
+                  }
+   "version"  "101"   ; @todo WTF
+   })
 
 
-(defn update-db [sync-table data]
-  (let [db-updates (into {} (for [[blip-id blip-data] (get-in data ["blips" "map"])
-                                  :let [db-str (blip-data-to-db-structure blip-id blip-data)]
-                                  :when (not= (first db-str) log-reploc)] db-str))]
-    (swap! db merge db-updates)
-    (json-str (log-opers-to-json (updates-to-log-opers sync-table
-                                   (into {} (for [db-update db-updates
-                                                  :when (containsex?
-                                                          (for [event (get-in data ["events" "list"])
-                                                                :when (not= (log (event "modifiedBy")) "panda@a.gwave.com")
-                                                                :when (= (event "type") "BLIP_SUBMITTED")]
-                                                            (get-in event ["properties" "map" "blipId"]))
-                                                          ((key db-update) :blip-id))] db-update))))))) ; @todo forex not force db-update at end
+
+; ===================================
+; ======= rep-rules utilities =======
+; ===================================
+
+; @todo: what is db-loc?
+
+(defn rep-by-text! [text]
+  (let [rep-class (into #{}
+                    (for [[db-loc content] @db
+                          :when (.contains content text)] db-loc))]
+    (swap! rep-rules conj rep-class)
+    rep-class))
 
 
-(defn sync-with-text [text]
-  (let [sync-class (disj
-                           (set (keys (filter (fn [[key val]] (.contains (val "content") text)) @db)))
-                           log-reploc)]
-    (swap! sync-table conj sync-class)
-    sync-class))
 
+; ==========================
+; ======= Web Server =======
+; ==========================
+
+
+(defn update-db! [rep-ops]
+  (swap! db into
+    (for [rep-op rep-ops] [(:rep-loc rep-op) (:content rep-op)])))
 
 (defroutes greeter
   (ANY "/wave"
-    (let [data (read-json (params :events))]
-      (update-db @sync-table data))))
+    (json-str
+      (rep-ops-to-outgoing-map
+        (do-replication @rep-rules
+          (incoming-map-to-rep-ops
+            (read-json
+              (params :events))))))))
+
+
+; @TODO WHEN DO WE CALL UPDATE-DB?!$@
+
 
 (run-server {:port 31337}
   "/*" (servlet greeter))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
